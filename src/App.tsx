5335dcddd0d6609c951479f3300c270b08ac49ae
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2022-12-13 20:17:38 +0000

    torus

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,16 @@ import React from "react";
 // import 'canvas' from react three fibre (assumably different from other canvases)
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { useLoader } from '@react-three/fiber'
+
+
 
 
 function App():JSX.Element{
 
+  const pokeball = useLoader(OBJLoader, '../components/quikPokeball.obj')
+  
   return(
     <div className="canvas-container">
       <Canvas> 
@@ -14,6 +20,8 @@ function App():JSX.Element{
         <ambientLight intensity={0.1} />
         <directionalLight color="blue" position={[2,3,5]} />
         <directionalLight color="yellow" position={[-5,-2,-1]} />
+
+        <primitive object={pokeball} />
 
         {/* An React component different from 'canvas' - unseen UI while undefined */}
         {/* Code is eqivalent to:*/}
@@ -40,7 +48,7 @@ function App():JSX.Element{
         */}
         <mesh>
           {/* Should be drawn unseen by HTML */}
-          <boxGeometry args={[2,5,2]} />
+          <torusGeometry args={[10,3,16,100]} />
           {/* Research but assuming is the function that generates a (default) box
            and the below adds a material to colour/texture it */}
           <meshStandardMaterial />
