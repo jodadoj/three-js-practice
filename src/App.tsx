4b2a9f15b8918d2c3a9c5514d946c75d7f67543c
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2022-12-16 11:45:07 +0000

    attempting rapier

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,59 @@ import { OrbitControls, Stage } from "@react-three/drei";
 import { useGLTF } from "@react-three/drei";
 import { Suspense } from 'react'
 import { Float } from "@react-three/drei";
+import PhyPlane from "./physPlane";
+
+import { Physics } from "@react-three/cannon";
+
+import { useThree } from "@react-three/fiber";
+
+import { Pokeball } from "./pokeball"; 
 
 
+import { Physics, RigidBody, Debug } from "@react-three/rapier";
 
-function toggle(currentValue:boolean):boolean{
- return !currentValue;
+function more(currentArr:number[]):number[] {
+  if (currentArr.length < 30){
+    currentArr.push(1); 
+  }
+  return currentArr;
 }
+function less(currentArr:number[]):number[] {
+  if (currentArr.length > 0){
+    currentArr.pop();
+  }
+  return currentArr;
+}
+
+
 
 function App():JSX.Element{
 
-  const [isBox, setIsBox] = useState(true);
-
-  const pokeball = useGLTF('/models/quikPokeball.gltf');
-
+  //const pokeball = useGLTF('/models/quikPokeball.gltf');
+  
   return(
     <div className="canvas-container">
-      <Canvas> 
+      <Canvas
+      
+  onClick={(e: any) => console.log('click')}
+  onContextMenu={(e: any) => console.log('context menu')}
+  onDoubleClick={(e: any) => console.log('double click')}
+  onWheel={(e: any) => console.log('wheel spins')}
+  onPointerMissed={() => console.log('missed')}
+      > 
         <OrbitControls />
+        <ambientLight intensity={1}/>
         
-        <Stage>
-          <Suspense fallback={null}>
-            <Float><primitive object={pokeball.scene} /></Float>
-          </Suspense>
-        </Stage>
-
+          <Physics gravity={[0, -9.81, 0]}>
+              <Pokeball position={[3-Math.random()*6,3-Math.random()*6,3-Math.random()*6]} />
+              <Pokeball position={[3-Math.random()*6,3-Math.random()*6,3-Math.random()*6]} />
+              <Pokeball position={[3-Math.random()*6,3-Math.random()*6,3-Math.random()*6]} />
+              <PhyPlane
+                    color={0x75e6da}
+                    position={[0, -5, 0]}
+                    rotation={[-Math.PI / 2, 0, 0]}
+                />
+          </Physics>
       </Canvas>
     </ div>
   );
---
+++ src/physPlane.js
@@ -0,0 +1,12 @@
+import { usePlane } from "@react-three/cannon";
+import { Plane } from "@react-three/drei";
+
+export default function PhyPlane({ color, ...props }) {
+    const [ref] = usePlane(() => ({ ...props }));
+  
+    return (
+        <Plane args={[1000, 1000]} ref={ref} position={[0,0,-10]}>
+          <meshStandardMaterial color={color} />
+        </Plane>
+    );
+  }
